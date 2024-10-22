116b07cc6793921f1e5ec6edf316ce1d9e6cce5f
rm40405 <rm404085@gmail.com>
2024-10-22 11:56:14 +0600

    start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './component/Header/Header'
 
 function App() {
  
   return (
     <>
       
-      <h1 className="text-sky-300">Knowladge Cafe</h1>
+     <Header></Header>
       
     </>
   )
